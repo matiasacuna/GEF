2ba37156f526a1c3971829e8a971f9e93ea16031
marijara <marijara@alumnos.uai.cl>
2016-05-11 20:03:36 -0300

    Tabs para seleccionar material por materia

--- index.php
+++ index.php
@@ -1,60 +1,48 @@
+
 <!DOCTYPE html>
-<html lang="es">
-
-<head>
-<meta charset="utf-8">
-<title>Proyecto GEF - Login de prueba</title>
-
-<!-- Google Fonts -->
-<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>
-
-<link rel="stylesheet" href="css/animate.css">
-<!-- Custom Stylesheet -->
-<link rel="stylesheet" href="css/style.css">
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-</head>
-
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 <body>
-<div class="container">
-<div class="top">
-<h1 id="title" class="hidden"><span id="logo">Proyecto <span>GEF</span></span></h1>
-</div>
-<div class="login-box animated fadeInUp">
-<div class="box-header">
-<h2>Inicia Sesion</h2>
-</div>
-<label for="username">Usuario</label>
-<br/>
-<input type="text" id="username">
-<br/>
-<label for="password">Contraseña</label>
-<br/>
-<input type="password" id="password">
-<br/>
-<button type="submit">Inicia Sesión</button>
-<br/>
 
+<div class="w3-container">
+<h2>Asignaturas</h2>
+<p>Selecciona la asignatura en la cual desees trabajar</p>
 </div>
+
+<ul class="w3-navbar w3-black">
+<li><a href="#" onclick="openCity('Lenguaje')">Lenguaje</a></li>
+<li><a href="#" onclick="openCity('Matematicas')">Matematicas</a></li>
+<li><a href="#" onclick="openCity('Ciencias')">Ciencias</a></li>
+</ul>
+
+<div id="Lenguaje" class="w3-container city">
+<h2>Lenguaje</h2>
+<p>Material para ortografia</p>
 </div>
-</body>
+
+<div id="Matematicas" class="w3-container city">
+<h2>Matematicas</h2>
+<p>Material de algebra</p>
+</div>
+
+<div id="Ciencias" class="w3-container city">
+<h2>Ciencias</h2>
+<p>Material de biologia</p>
+</div>
 
 <script>
-$(document).ready(function () {
-	$('#logo').addClass('animated fadeInDown');
-	$("input:text:visible:first").focus();
-});
-	$('#username').focus(function() {
-		$('label[for="username"]').addClass('selected');
-	});
-		$('#username').blur(function() {
-			$('label[for="username"]').removeClass('selected');
-		});
-			$('#password').focus(function() {
-				$('label[for="password"]').addClass('selected');
-			});
-				$('#password').blur(function() {
-					$('label[for="password"]').removeClass('selected');
-				});
-					</script>
-					
+openCity("London")
+function openCity(cityName) {
+	var i;
+	var x = document.getElementsByClassName("city");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	document.getElementById(cityName).style.display = "block";
+}
+</script>
+
+</body>
+</html>
