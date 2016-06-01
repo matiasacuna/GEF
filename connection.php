4057d843ceb39bed948552b53c8618ba5336cc06
Matias <Matias@10.50.127.76>
2016-06-01 19:48:43 -0300

    Actualizados

--- connection.php
+++ connection.php
@@ -5,6 +5,6 @@ define('DB_PASSWORD', '');
 define('DB_DATABASE', 'gefdb');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 function formatDate($date){
-	return date('g:i a', strtotime($date));
+return date('g:i a', strtotime($date));
 }
 ?>
